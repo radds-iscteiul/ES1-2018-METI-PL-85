7d88d6a44ed395d994b6568bd146f06e765bb193
Rafael Dias <radds@iscte-iul.pt>
2018-10-06 22:43:39 +0100

    Test1

--- ES_BomDiaAcademia/src/config/XMLManager.java
+++ ES_BomDiaAcademia/src/config/XMLManager.java
@@ -18,6 +18,7 @@ import engine.Service;
 
 public class XMLManager {
 	    /**
+	     * Reads a .xml file
 	     * @param args the command line arguments
 	     * @throws ParserConfigurationException 
 	     * @throws IOException 
@@ -105,5 +106,6 @@ public class XMLManager {
 	    }
 	
 
+	    
 
 }
