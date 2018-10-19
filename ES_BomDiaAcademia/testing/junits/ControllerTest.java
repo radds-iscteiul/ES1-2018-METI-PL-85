4e5e96f85c40372851502a1bc3ed69607fd13299
dmcmo-iscteiul <dmcmo@iscte-iul.pt>
2018-10-19 19:32:02 +0100

    Library Fix Twitter

--- ES_BomDiaAcademia/testing/junits/ControllerTest.java
+++ ES_BomDiaAcademia/testing/junits/ControllerTest.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.junit.Test;
 
 import engine.Controller;
+import engine.EmailMessage;
 import engine.MyMessage;
 
 public class ControllerTest {
@@ -14,7 +15,7 @@ public class ControllerTest {
 	@Test
 	public void wordFilterTest() {
 		
-		MyMessage m = new MyMessage("Rafael", "Inês", new Date(), "Test", "Search for a word in this message");
+		MyMessage m = new EmailMessage("Rafael", "Inês", "Test", new Date(), "Search for a word in this message");
 		String palavra = "Search for";
 		
 		assertEquals(Controller.getInstance().wordFilter(m, palavra), true); 
