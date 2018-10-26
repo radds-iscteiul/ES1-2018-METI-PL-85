dbc4c5540ef1285d98c79e60473f858ba5e41c72
Rafael Dias <radds@iscte-iul.pt>
2018-10-26 22:35:50 +0100

    Escrita de XML

--- ES_BomDiaAcademia/src/config/XMLManager.java
+++ ES_BomDiaAcademia/src/config/XMLManager.java
@@ -1,12 +1,19 @@
 package config;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -96,6 +103,69 @@ public class XMLManager {
 	    }
 	
 
+	    public void writeXML(ArrayList<Service> servicos) throws TransformerException, ParserConfigurationException {
+	    	DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+	    	DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+	    	
+	    	Document configXML = documentBuilder.newDocument();
+	    	
+	    	Element root = configXML.createElement("servidores");
+	    	configXML.appendChild(root);
+	    	
+	    	Element s;
+	    	int id = 1;
+	    	for (Service service : servicos) {
+	    		s = this.composeServiceToXML(service, configXML,id);
+		    	root.appendChild(s);
+		    	id++;
+			}
+	    	
+	    	
+	    	
+	    	TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    	Transformer transformer = transformerFactory.newTransformer();
+	    	
+	    	DOMSource documentoFonte = new DOMSource(configXML);
+	    	StreamResult documentoFinal = new StreamResult(new File("C:\\Users\\Rafael Dias\\Desktop\\teste.xml"));
+	    	
+	    	transformer.transform(documentoFonte, documentoFinal);
+	    }
 	    
+	    private Element composeServiceToXML(Service s, Document configXML,int idNumber) {
+	    	Element service = configXML.createElement("servidor");
+	    	Attr id = configXML.createAttribute("id");
+	    	id.setValue(String.valueOf(idNumber));
+	    	service.setAttributeNode(id);
+	    
+	    	
+	    	Element nome = configXML.createElement("nome");
+	    	nome.appendChild(configXML.createTextNode(s.getName().toString())); 	
+	    	service.appendChild(nome);
+	    	
+	    	Element endereco = configXML.createElement("endereco");
+	    	endereco.appendChild(configXML.createTextNode(s.getEndereco())); 	
+	    	service.appendChild(endereco);
+	    	
+	    	Element utilizador = configXML.createElement("utilizador");
+	    	utilizador.appendChild(configXML.createTextNode(s.getUser())); 	
+	    	service.appendChild(utilizador);
+	    	
+	    	Element password = configXML.createElement("password");
+	    	password.appendChild(configXML.createTextNode(s.getPassword())); 	
+	    	service.appendChild(password);
+	    	
+	    	return service;
+	    }
 
+	    public static void main(String[] args) {
+	    	
+	    	ArrayList<Service> services = new ArrayList<Service>();
+	    	services.add(new Service(1, "Email", "0.0.0.0", "a@gmail.com", "password"));
+			try {
+				new XMLManager().writeXML(services);
+			} catch (TransformerException | ParserConfigurationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 }
